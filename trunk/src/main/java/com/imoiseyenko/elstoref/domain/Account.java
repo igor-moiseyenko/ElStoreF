844a0a92698a49a2f9bdf219e14a4fafc59f2884
Igor Moiseyenko <imoiseyenko93@gmail.com>
2013-12-08 22:19:00 +0300

    Added validation annotations to the Account entity. But, still not work...

--- trunk/src/main/java/com/imoiseyenko/elstoref/domain/Account.java
+++ trunk/src/main/java/com/imoiseyenko/elstoref/domain/Account.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name="Account", schema="dbo")
@@ -14,9 +16,16 @@ import javax.persistence.Table;
 public class Account extends DomainObject {
 
 	private static final long serialVersionUID = 1L;
-
+	
+	@Size(min=3, max=20, message="Username must be between 3 and 20 characters long.")
+	@Pattern(regexp="^[a-zA-Z0-9_]+$", message="Username must be alphanumeric with no spaces.")
 	private String username;
+	
+	@Size(min=6, max=20, message="The password must be between 3 and 20 characters long.")
 	private String password;
+	
+	@Pattern(regexp="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}",
+			message="Invalid email address.")
 	private String email;
 	
 	public Account () {}
