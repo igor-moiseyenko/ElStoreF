8e0a7f3158765e1d98f7dc6cdb6cd3372735935d
Igor Moiseyenko <imoiseyenko93@gmail.com>
2013-12-10 02:35:24 +0300

    Added fields. Added named query to find all players.

--- trunk/src/main/java/com/imoiseyenko/elstoref/domain/inventoryItem/Player.java
+++ trunk/src/main/java/com/imoiseyenko/elstoref/domain/inventoryItem/Player.java
@@ -1,16 +1,51 @@
 package com.imoiseyenko.elstoref.domain.inventoryItem;
 
 import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 @Entity
+@NamedQueries({
+	@NamedQuery(name="Player.findAllPlayers", query="SELECT p FROM Player p")
+})
 public class Player extends InventoryItem {
 	
 	private static final long serialVersionUID = 1L;
+	
+	private String caseMaterial;
+	
+	public Player () {}
+	
+	public String getCaseMaterial () {
+	
+		return caseMaterial;
+	}
+	
+	public void setCaseMaterial (String caseMaterial) {
+	
+		this.caseMaterial = caseMaterial;
+	}
 
 	@Override
 	public boolean equalsByFields (Object obj) {
 
-		// TODO Auto-generated method stub
+		if (this == obj) {
+			
+			return true;
+		}
+		if ((obj == null) || (this.getClass() != obj.getClass())) {
+			
+			return false;
+		}
+		
+		Player other = (Player) obj;
+		
+		if (super.equalsByFields(other)
+				&& this.caseMaterial.equals(other.caseMaterial)) {
+			
+			return true;
+		}
+		
 		return false;
 	}
 
