ad0daca3f6b8ad888dc004ad260f00c61f52ebdb
Igor Moiseyenko <imoiseyenko93@gmail.com>
2013-12-10 02:08:00 +0300

    Added controller to handle requests to mobile catalog.

---
+++ trunk/src/main/java/com/imoiseyenko/elstoref/controller/catalog/MobileCatalogController.java
@@ -0,0 +1,28 @@
+package com.imoiseyenko.elstoref.controller.catalog;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.imoiseyenko.elstoref.iservice.IMobilePhoneService;
+
+@Controller
+@RequestMapping("catalog/mobile")
+public class MobileCatalogController {
+
+	private static final String CATALOG_NAME = "Mobile catalog";
+	
+	@Autowired
+	private IMobilePhoneService mobilePhoneService;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String showMobileCatalogPage (Model model) {
+		
+		model.addAttribute("catalogName", CATALOG_NAME);
+		model.addAttribute("inventoryItemList", mobilePhoneService.findAllMobilePhones());
+		
+		return "inventory_item_catalog";
+	}
+}
