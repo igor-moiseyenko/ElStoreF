bee9507dca4444a84d333fb1e8a358e089770d72
Igor Moiseyenko <imoiseyenko93@gmail.com>
2013-12-13 23:58:27 +0300

    Added entities 'Order' and 'OrderItem', established relationships between them.
    Implemented method 'equalsByFields' for both objects.
    
    In case of OrderItem: is it need to compare order fields of 'this' and
    'other' in the 'equalsByFields' method?

--- trunk/src/main/java/com/imoiseyenko/elstoref/domain/Order.java
+++ trunk/src/main/java/com/imoiseyenko/elstoref/domain/Order.java
@@ -1,14 +1,93 @@
 package com.imoiseyenko.elstoref.domain;
 
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="\"Order\"", schema="dbo")
 public class Order extends DomainObject {
 
 	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy="order", cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+	private List<OrderItem> orderItems;
+	
+	private Date date;
+	private double totalAmount;
+	private String state;
+	
+	public Order () {}
+	
+	public void addOrderItem (OrderItem orderItem) {
+		
+		orderItem.setOrder(this);
+		this.orderItems.add(orderItem);
+	}
+	
+	public void removeOrderItem (OrderItem orderItem) {
+		
+		this.orderItems.remove(orderItem);
+		orderItem.setOrder(null);
+	}
+	
+	public Date getDate () {
+	
+		return date;
+	}
+	
+	public void setDate (Date date) {
+	
+		this.date = date;
+	}
+	
+	public double getTotalAmount () {
+	
+		return totalAmount;
+	}
+	
+	public void setTotalAmount (double totalAmount) {
+	
+		this.totalAmount = totalAmount;
+	}
+	
+	public String getState () {
+	
+		return state;
+	}
+	
+	public void setState (String state) {
+	
+		this.state = state;
+	}
 
 	@Override
 	public boolean equalsByFields (Object obj) {
 
-		// TODO Auto-generated method stub
+		if (this == obj) {
+			
+			return true;
+		}
+		if ((obj == null) || (obj.getClass() != this.getClass())) {
+			
+			return false;
+		}
+		
+		Order other = (Order) obj;
+		
+		if (this.date.equals(other.date)
+				&& this.totalAmount == other.totalAmount
+				&& this.state.equals(other.state)
+				&& this.orderItems.equals(other.orderItems)) {
+			
+			return true;
+		}
+		
 		return false;
 	}
-
 }
--- trunk/src/main/java/com/imoiseyenko/elstoref/domain/OrderItem.java
+++ trunk/src/main/java/com/imoiseyenko/elstoref/domain/OrderItem.java
@@ -1,13 +1,77 @@
 package com.imoiseyenko.elstoref.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+import com.imoiseyenko.elstoref.domain.inventoryItem.InventoryItem;
+
+@Entity
 public class OrderItem extends DomainObject {
 
 	private static final long serialVersionUID = 1L;
+	
+	@ManyToOne
+	private Order order;
+	
+	@OneToOne
+	private InventoryItem inventoryItem;
+	
+	private int quantity;
+	
+	public OrderItem () {}
+	
+	public Order getOrder () {
+	
+		return order;
+	}
+	
+	public void setOrder (Order order) {
+	
+		this.order = order;
+	}
+	
+	public InventoryItem getInventoryItem () {
+	
+		return inventoryItem;
+	}
+	
+	public void setInventoryItem (InventoryItem inventoryItem) {
+	
+		this.inventoryItem = inventoryItem;
+	}
+	
+	public int getQuantity () {
+	
+		return quantity;
+	}
+	
+	public void setQuantity (int quantity) {
+	
+		this.quantity = quantity;
+	}
 
 	@Override
 	public boolean equalsByFields (Object obj) {
 
-		// TODO Auto-generated method stub
+		if (this == obj) {
+			
+			return true;
+		}
+		if ((obj == null) || (obj.getClass() != this.getClass())) {
+			
+			return false;
+		}
+		
+		OrderItem other = (OrderItem) obj;
+		
+		// Is it need to compare 'order' field or not?
+		if (this.inventoryItem.equalsByFields(other.inventoryItem)
+				&& this.quantity == other.quantity) {
+			
+			return true;
+		}
+		
 		return false;
 	}
 
